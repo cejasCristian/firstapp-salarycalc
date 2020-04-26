ac5ae1c3a4a232d4f241de564882864779b29d60
cristian <cejasristianh@gmail.com>
2020-04-26 19:00:45 -0300

    Updates

---
+++ precache-manifest.869d7909c69ad4522922413da174586f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d791b99d8c59184803837a9cdf7b94",
+    "url": "/firstapp-salarycalc/index.html"
+  },
+  {
+    "revision": "682589c93bb1790ec581",
+    "url": "/firstapp-salarycalc/static/css/main.85806d2f.chunk.css"
+  },
+  {
+    "revision": "0a293770f48503a2be61",
+    "url": "/firstapp-salarycalc/static/js/2.ef1b9c72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstapp-salarycalc/static/js/2.ef1b9c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682589c93bb1790ec581",
+    "url": "/firstapp-salarycalc/static/js/main.d959432c.chunk.js"
+  },
+  {
+    "revision": "c3c0fc43de053582285b",
+    "url": "/firstapp-salarycalc/static/js/runtime-main.8792ddc1.js"
+  }
+]);
